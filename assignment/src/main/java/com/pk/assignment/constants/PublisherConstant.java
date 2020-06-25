804809c72cdb3d8be5c820363d957cb16db3caac
KhazaShaik <khaza.shaik8@t-mobile.com>
2020-06-25 10:28:20 +0530

    Revert "Add files via upload"
    This reverts commit f6c0470feba5d03b9ee3050d4fad9ab3314dcdef.

--- assignment/src/main/java/com/pk/assignment/constants/PublisherConstant.java
+++
@@ -1,7 +0,0 @@
-package com.pk.assignment.constants;
-
-public class PublisherConstant {
-    public static final String NUMBER_MASK = ".(?=.{4,}$)";
-    public static final String EMAIL_MASK = "(?<=.{3}).(?=.*@)";
-
-}
